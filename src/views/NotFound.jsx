bfab00ac9363e9cd21c54bea8e8c2573d2642582
Fleepx <felipesanchez.aq@gmail.com>
2024-08-27 20:57:06 -0400

    Ultimo

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <footer className= "text-center mt-5">
-      <h1>ERROR 404</h1>
+      <h1>*ERROR 404*</h1>
       <p>pagina no encontrada</p>
     </footer>
   );
